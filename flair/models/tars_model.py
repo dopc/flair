cdb7db478eaa3d5285d2e5888a23b075af71ff33
dopc <aliacar23@gmail.com>
2023-01-04 19:33:16 +0300

    label threshold

--- flair/models/tars_model.py
+++ flair/models/tars_model.py
@@ -267,6 +267,7 @@ class FewshotClassifier(flair.nn.Classifier[Sentence]):
         sentences: Union[List[Sentence], Sentence],
         candidate_label_set: Union[List[str], Set[str], str],
         multi_label: bool = True,
+        multi_label_threshold = 0.5
     ):
         """
         Method to make zero shot predictions from the TARS model
@@ -303,7 +304,7 @@ class FewshotClassifier(flair.nn.Classifier[Sentence]):
 
         try:
             # make zero shot predictions
-            self.predict(sentences)
+            self.predict(sentences, label_threshold=multi_label_threshold)
         finally:
             # switch to the pre-existing task
             self.switch_to_task(existing_current_task)
